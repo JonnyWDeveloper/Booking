1eb99100bad8a776b6073640169bb5d023d6070d
Jonny Walter <jonny.walter@ownit.nu>
2023-01-24 23:18:15 +0100

    Add project files.

---
+++ Booking.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Booking.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.Data.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<GymClass> GymClasses => Set<GymClass>();
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUserGymClass>().HasKey(a => new { a.ApplicationUserId, a.GymClassId });
+
+            builder.Entity<GymClass>().HasQueryFilter(g => g.StartTime > DateTime.UtcNow);
+        }
+    }
+}
